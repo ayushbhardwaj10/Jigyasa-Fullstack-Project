ce0b2865a336db8cff7b5f290f1bcc43caa8d909
ayush <ayush@gmail.com>
2022-02-27 19:24:09 +0530

    question component partially done

--- jigyasaProject/src/app/app.module.ts
+++ jigyasaProject/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PostQuestionComponent } from './components/post-question/post-question.component';
+import { ReplyQuestionComponent } from './components/reply-question/reply-question.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, PostQuestionComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, PostQuestionComponent, ReplyQuestionComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ jigyasaProject/src/app/components/reply-question/reply-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reply-question',
+  templateUrl: './reply-question.component.html',
+  styleUrls: ['./reply-question.component.css']
+})
+export class ReplyQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
