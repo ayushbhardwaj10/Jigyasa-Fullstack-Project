fbdab4165de7a1b8b23656de6f1aa8550c1eac2c
ayush <ayush@gmail.com>
2022-03-01 18:10:58 +0530

    reply question component design done

--- jigyasaProject/src/app/components/post-question/post-question.component.ts
+++ jigyasaProject/src/app/components/post-question/post-question.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-question',
   templateUrl: './post-question.component.html',
-  styleUrls: ['./post-question.component.css']
+  styleUrls: ['./post-question.component.css'],
 })
 export class PostQuestionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  code: any;
+  postQuestion() {
+    //code to store code in HTML tags and load in a target div with css property of =>  white-space: pre-wrap;
+    /*     console.log(
+      (document.getElementById('sampleCode') as HTMLInputElement).value
+    );
+    this.code = (
+      document.getElementById('sampleCode') as HTMLInputElement
+    ).value;
 
-  ngOnInit(): void {
+    document.querySelectorAll('.display-content')[0].innerHTML = this.code; */
   }
-
 }
--- jigyasaProject/src/app/components/reply-question/reply-question.component.ts
+++ jigyasaProject/src/app/components/reply-question/reply-question.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-reply-question',
   templateUrl: './reply-question.component.html',
-  styleUrls: ['./reply-question.component.css']
+  styleUrls: ['./reply-question.component.css'],
 })
 export class ReplyQuestionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  questionCode =
+    '<p>class MyComponent {</p> <p>constructor(){ </p> <p>// how to query the DOM element from here? }} </p><p>}}</p>';
+  answerCode1 =
+    '<p>"scripts": [</p><p>  "node_modules/jquery/dist/jquery.min.js"</p><p>  ]</p>';
+  answerCode2 = '<p>install jquery@latest';
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
